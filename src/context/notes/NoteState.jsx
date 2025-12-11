3aac6dedda92a78e71c10eff673cfec76fce5d5d
Amit Kumar Pandey <138184479+gituser1612@users.noreply.github.com>
2025-12-11 22:39:57 +0530

    Update backend host URL in NoteState component

--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -3,7 +3,7 @@ import NoteContext from "./noteContext";
 
 
 const NoteState = (props) => {
-  const host = "https://inotebook-backend-yb82.onrender.com"
+  const host = "https://inotebook-backend-1-6ylp.onrender.com"
   const notesInitial = []
   const [notes, setNotes] = useState(notesInitial)
 
